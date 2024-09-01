8440d76d0c842f442f646812b71b5c067c260287
欧子 <ouzi7997@163.com>
2024-09-01 20:47:41 +0800

    元组

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import python_base as pb
+# import语句用来导入其他python文件（称为模块module），使用该模块里定义的类、方法或者变量，从而达到代码复用的目的
 
 # 交互
 # pb.test(1)
@@ -11,3 +12,6 @@ import python_base as pb
 # pb.test3()
 # pb.test4()
 # pb.test5()
+
+# 元组
+pb.test6()
--- python_base.py
+++ python_base.py
@@ -3,7 +3,7 @@ def test(param):
     """
     函数说明
     :param param:
-    :return:
+    :return: void
     """
     msg = input('请输入：')
     print(param, msg)
@@ -11,6 +11,9 @@ def test(param):
 
 
 def test1():
+    """
+    for else  List（列表）
+    """
     sites = ["1", "2", "3", "4"]
     for site in sites:
         if site == "3":
@@ -25,7 +28,7 @@ def test1():
 
 def test2():
     """
-    for key in a  == for key in a.keys()
+    for key in a  == for key in a.keys()  字典(dictionary)
     """
     mp = {'a': '1', 'b': '2', 'c': '3'}
     for key in mp:
@@ -41,6 +44,7 @@ def test3():
 
 
 def test4():
+    # 输出元组
     mp = {'a': '1', 'b': '2', 'c': '3'}
     for item in mp.items():
         print(item)
@@ -55,5 +59,20 @@ def test5():
     mp = {'a': '1', 'b': '2', 'c': '3'}
     for key, value in mp.items():
         print(key, value)
+    return
+
+def test6():
+    """
+    元组  类似于只读的 List（列表）不能二次赋值
+    """
+    tuple = ( '1', 2 , 3.33, '4', 5.5 )
+    tinytuple = (6, '7.7')
+     
+    print (tuple)                      # 输出完整元组
+    print (tuple[0])                   # 输出元组的第一个元素
+    print (tuple[1:3])                 # 输出第二个至第四个（不包含）的元素 
+    print (tuple[2:])                  # 输出从第三个开始至列表末尾的所有元素
+    print (tinytuple * 3)              # 输出元组两次
+    print (tuple + tinytuple)          # 打印组合的元组
     return
 
